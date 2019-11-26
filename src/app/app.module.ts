f429d013adcd410d8a81217f5338baf52d7db977
Michael Musatov <mv.musatov@gmail.com>
2019-11-26 10:37:36 +0400

    Initial UI form to be validated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    ReactiveFormsModule,
+    AppComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule
