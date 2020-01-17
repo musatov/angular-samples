d1313ea7b6f9b51d0250401ad9b1206d4f0c3755
Michael Musatov <mv.musatov@gmail.com>
2020-01-17 12:11:15 +0400

    Single control validation sample is updated

--- form-validators/src/app/single-control/single-control.component.ts
+++ form-validators/src/app/single-control/single-control.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, ValidationErrors, Validators } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-single-control',
@@ -7,10 +9,26 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./single-control.component.scss']
 })
 export class SingleControlComponent {
-  readonly emailControl: FormControl;
-  constructor() {
-    this.emailControl = new FormControl(null, [Validators.required, Validators.email], [])
-  }
+  readonly emailControl = new FormControl(null, 
+    // Sync validators
+    [Validators.required, Validators.email],
+    // Async validators
+    [emailMustNotBeUsed, emailIsForbidden]);
 }
-// ToDo: Async Validator email used
 
+function emailMustNotBeUsed(control: AbstractControl): Observable<ValidationErrors | null> {
+  console.log('First async validator stars executing');
+  const resutl$ = control.value === 'used@email.com' 
+      ? of({'email-is-used': 'Email was used'}).pipe(delay(2000))
+      : of(null).pipe(delay(2000));
+  return resutl$.pipe(finalize(() => console.log('First async validator completed')));    
+}
+
+function emailIsForbidden(control: AbstractControl): Observable<ValidationErrors | null> {
+  console.log('Second async validator stars executing');
+  const resutl$ = control.value === 'forbidden@email.com' 
+      ? of({'email-is-forbidden': 'Email was forbidden'}).pipe(delay(1500))
+      : of(null).pipe(delay(1500));
+
+  return resutl$.pipe(finalize(() => console.log('Second async validator completed')));    
+}
